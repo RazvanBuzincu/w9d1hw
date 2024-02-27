834e6c9d2ac87faa215e823ab28ee38754761d01
RazvanBuzincu <razvanbuzincu@gmail.com>
2024-02-27 17:57:22 -0600

    'second commit'

---
+++ car_shop/src/firebaseConfing.ts
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyCoQBFGyo8J7jVip1qMuAxQWfw_PZd-OpQ",
+  
+    authDomain: "car-shophw.firebaseapp.com",
+  
+    projectId: "car-shophw",
+  
+    storageBucket: "car-shophw.appspot.com",
+  
+    messagingSenderId: "610994897828",
+  
+    appId: "1:610994897828:web:f4694cdfc3051eb0507731"
+  
+  };
+  
--- car_shop/src/main.tsx
+++ car_shop/src/main.tsx
@@ -2,22 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import { ThemeProvider } from "@mui/material/styles";
+import { FirebaseAppProvider } from 'reactfire'; 
+import 'firebase/auth';
+
 
 // internal imports 
-import { Home, Shop, Cart } from './components'; 
-import './index.css'
-import { theme } from './Theme/themes';
+import { Home, Shop, Cart, Auth } from './components'; 
+import './index.css';
+import { theme } from './Theme/themes'; 
+import { firebaseConfig } from './firebaseConfing';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme = { theme }>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home title = {"Best used cars in town"}/>} />
-          <Route path='/shop' element={<Shop />} />
-          <Route path='/cart' element={<Cart />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <ThemeProvider theme = { theme }>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home title = {"Best used cars in town!"}/>} />
+            <Route path='/auth' element={<Auth title = {"Best used cars in town!"}/>} /> 
+            <Route path='/shop' element={<Shop />} />
+            <Route path='/cart' element={<Cart />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </FirebaseAppProvider>
   </React.StrictMode>,
 )
